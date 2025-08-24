92474eb62d690b96d7b47521ec065aad5bfcb04b
nogringo <nogringo@users.noreply.github.com>
2025-08-24 15:28:21 +0000

    deploy: f6f8bf8287a01df3de5e3484639c2546e3878a7f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2244928468"
+    serviceWorkerVersion: "1943955485"
   }
 });
